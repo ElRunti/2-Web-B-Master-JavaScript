35b7229b006616ae0fc83e7119bb5ace53786455
ElRunti <ruben060805@outlook.com>
2026-01-29 19:30:41 -0600

    feat: variables y operadores

---
+++ variables.js
@@ -0,0 +1,44 @@
+let mascota = 'perro';
+let mascota1 = 'gato';
+
+const numero = 23;
+
+let num1 = 3;
+let num2 = 5;
+
+console.log("Operadores matematicos");
+let suma = num1 + num2;
+let resta = num2 - num1;
+let modulo = num2 % num1;
+let exp = num1 ** 2;
+console.log(suma);
+console.log(15+15);
+console.log(resta);
+console.log(modulo);
+console.log(exp);
+
+
+console.log("Operadores logicos")
+console.log(10 == 10);
+console.log(12 == '12');
+
+//comparacion estricta triple igual
+console.log(23 === '23'); 
+console.log("---------");
+let valor1 = 20;
+let valor2 = 30;
+
+//mayor que
+console.log(valor1 > valor2);
+//mayor o igual que
+console.log(valor1 >= valor2);
+//menor o igual que
+console.log(valor1 <= valor2);
+//menor que
+console.log(valor1 < valor2);
+//distinto que
+console.log(10 != '10');
+// distinto que estricto
+console.log(10 !== '10');
+
+
