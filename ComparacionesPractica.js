9d6e83d76574e0d2a39924a0ad5e0f5bf962ba2e
ElRunti <ruben060805@outlook.com>
2026-02-03 18:39:52 -0600

    feat: Practica:Comparaciones y decisiones en JavaScript

---
+++ ComparacionesPractica.js
@@ -0,0 +1,13 @@
+//let nota = prompt("Ingresa la calificacion"); ejemplo de pedir el dato 
+
+let nota = 85; // ejemplo de dato ya definido
+
+if(nota >= 90){
+    console.log("Exelente");
+}else if (nota >= 75 && nota <= 89) {
+    console.log("Bien");
+}else if (nota >= 60 && nota <= 74) {
+    console.log("Suficiente");
+}else {
+    console.log("No aprueba");
+}
