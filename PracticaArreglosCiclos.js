da6eccf96ddbaa2cbd63475ac844b43849f0f054
ElRunti <ruben060805@outlook.com>
2026-02-18 22:06:09 -0600

    feat: Practica Arreglos y cliclos

---
+++ PracticaArreglosCiclos.js
@@ -0,0 +1,12 @@
+let frutas = ["manzana","pera","papaya","platano"];
+
+
+for(let conteoFrutas = 0; conteoFrutas < frutas.length; conteoFrutas++){
+    console.log(`Fruta numero ${conteoFrutas + 1}. ${frutas[conteoFrutas]} `)
+}
+let conteoFrutasWhile = 0;
+while(conteoFrutasWhile < frutas.length){
+    console.log(`Fruta numero ${conteoFrutasWhile + 1}. ${frutas[conteoFrutasWhile]} `);
+
+    conteoFrutasWhile++;
+}
